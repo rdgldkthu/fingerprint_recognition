248eb3760b55dfe473c1c227fc9539d28ebd71b0
rdgldkthu <rdg22@mails.tsinghua.edu.cn>
2025-11-24 14:29:37 +0900

    feat: add frequency image estimation

--- include/fingerprint/enhancement.hpp
+++ include/fingerprint/enhancement.hpp
@@ -11,26 +11,39 @@ public:
   cv::Mat enhance(const cv::Mat &input) const;
 
 private:
-  const int block_size;
-  const int kernel_size;
-
-  cv::Mat estimateOrientation(const cv::Mat &img) const;
-  cv::Mat estimateFrequency(const cv::Mat &img,
-                            const cv::Mat &orientation_img) const;
-  cv::Mat generateRegionMask(const cv::Mat &img) const;
+  // === Preprocessing ===
   void normalize(const cv::Mat &src, cv::Mat &dst, double dmean = 100,
                  double dstd0 = 100) const;
+
+  // === Orientation ===
+  cv::Mat estimateOrientation(const cv::Mat &img) const;
+  void computeGradients(const cv::Mat &img, cv::Mat &grad_x,
+                        cv::Mat &grad_y) const;
+  void convertSinCos(const cv::Mat &img, cv::Mat &sin_img,
+                     cv::Mat &cos_img) const;
+
+  // === Frequency ===
+  cv::Mat estimateFrequency(const cv::Mat &img,
+                            const cv::Mat &orientation_img) const;
+  float computeBlockFrequency(const cv::Mat &img, float ori, int cy,
+                              int cx) const;
+  float estimatePeriod(const std::vector<float> &x_sig) const;
+
+  // === Region Mask ===
+  cv::Mat generateRegionMask(const cv::Mat &img) const;
+
+  // === Enhancement ===
   void applyGabor(const cv::Mat &src, cv::Mat &dst,
                   const cv::Mat &orientation_img,
                   const cv::Mat &frequency_img) const;
+
+  // === Debug/Visualization ===
   void showOrientation(const cv::Mat &bg_img, const cv::Mat &orientation_img,
                        const char *winname = "Orientation") const;
 
-  // Static Member Functions
-  static void calculateGradients(const cv::Mat &img, cv::Mat &grad_x,
-                                 cv::Mat &grad_y);
-  static void convertSinCos(const cv::Mat &img, cv::Mat &sin_img,
-                            cv::Mat &cos_img);
+  const int block_size;
+  const int kernel_size;
+  const int window_length = 2 * block_size;
 };
 
 } // namespace fp
