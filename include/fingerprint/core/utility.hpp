5394bde19fce4b249e06e462a8356d8eb28902d0
rdgldkthu <rdg22@mails.tsinghua.edu.cn>
2026-02-07 16:43:24 +0900

    refactor: update utility

--- include/fingerprint/core/utility.hpp
+++ include/fingerprint/core/utility.hpp
@@ -3,6 +3,15 @@
 
 namespace fp {
 
+static const int dx[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+static const int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+inline float normalizeAngle(float theta) {
+  while (theta >= CV_PI) theta -= 2 * CV_PI;
+  while (theta < -CV_PI) theta += 2 * CV_PI;
+  return theta;
+}
+
 inline float angleDiff(float a, float b) {
   float d = std::fabs(a - b);
   return std::min(d, float(CV_PI) - d);
